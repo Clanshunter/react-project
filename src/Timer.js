49d00de7aa4ba1f397526fce13e3ed0fdbe70076
Hossein <Hossein@PC-1>
2023-12-03 21:49:23 +0100

    component lifecycle

--- src/Timer.js
+++ src/Timer.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-class Timer extends React.Component{
-    constructor(){
-      super();
-      this.state={
-        time: new Date().toLocaleTimeString()
-      }
-    }
-    render(){
-      setInterval(() => {
-        this.setState({
-          time:new Date().toLocaleTimeString()
-        })
-      }, 1000);
-      return(
-        <h2 className='timer'>it is {this.state.time}</h2>
-            )
-    }
+var interval;
+class Timer extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      time: new Date().toLocaleTimeString(),
+    };
   }
 
-  export default Timer;
+  componentDidMount() {
+    interval = setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+    }, 1000);
+  }
+
+  render() {
+    return <h2 className="timer">it is {this.state.time}</h2>;
+  }
+}
+
+export default Timer;
